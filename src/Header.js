b1a5afadc870ebc1a69ce2334fc4fa1bd4857f1c
Haind <nduchai1998@gmail.com>
2020-10-12 20:47:34 +0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Header.css'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import StorageIcon from '@material-ui/icons/Storage';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img alt="" src="https://dominos.vn/Data/Sites/1/media/default2/img/logo.png"/>
+            </div>
+            <div className="header__right">
+                <div className="header__option">                    
+                    <AccountCircleIcon/>
+                </div>
+                <h3>ĐĂNG KÝ</h3>
+                <p>/</p>
+                <h3>ĐĂNG NHẬP</h3>
+                <div className="header__option">
+                    <FacebookIcon/>
+                </div>
+                <div className="header__option">
+                    <InstagramIcon/>
+                </div>
+                <h3>THEO DÕI ĐƠN HÀNG</h3>
+                <div className="header__option">
+                    <ShoppingBasketIcon/>
+                </div>
+                <div className="header__option">
+                    <StorageIcon/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
