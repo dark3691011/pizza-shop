74cca645c67cac8a0daae824e7fdc7a4123be4c4
Haind <nduchai1998@gmail.com>
2020-10-15 22:02:18 +0700

    'firebas.js'

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBoKpQ9sozNeIMfZLubhXrxOdhsFVQrPhE",
+    authDomain: "pizza-shop-65fc9.firebaseapp.com",
+    databaseURL: "https://pizza-shop-65fc9.firebaseio.com",
+    projectId: "pizza-shop-65fc9",
+    storageBucket: "pizza-shop-65fc9.appspot.com",
+    messagingSenderId: "509167953530",
+    appId: "1:509167953530:web:eacd3c19253437bec1bdc3",
+    measurementId: "G-RQJPC28E0J"
+  };
