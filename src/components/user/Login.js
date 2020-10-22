2a5475a65b25c83fba1024163478a611b2a528fe
Haind <nduchai1998@gmail.com>
2020-10-22 21:39:23 +0700

    add more structure

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -7,10 +7,14 @@ function Login() {
             <form>
                 <h1>ĐĂNG NHẬP</h1>
                 <h4>Thông tin đăng nhập</h4>
-                <p>EMAIL</p>
-                <input type="text"/>
-                <p>MẬT KHẨU</p>
-                <input type="text"/>
+                <div className="login__form__group">
+                    <label htmlFor="email">EMAIL</label>
+                    <input type="text" name="email" id="email"/>
+                </div>
+                <div className="login__form__group">
+                    <label htmlFor="password">MẬT KHẨU</label>
+                    <input type="text" name="password" id="password"/>
+                </div>
                 <button type="submit">ĐĂNG NHẬP</button>
             </form>
         </div>
