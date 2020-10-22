f160292400283183ee2f05011fa34507fbcaabbc
Haind <nduchai1998@gmail.com>
2020-10-22 21:37:54 +0700

    shortened code

--- src/components/category/ButtonTop.js
+++ src/components/category/ButtonTop.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import './ButtonTop.css'
+import './ButtonTop.css';
 
-ButtonTop.protoTypes = {
-    name: PropTypes.string
-}
-
-ButtonTop.defaultProps = {
-    name: "PIZZA"
-}
-
-function ButtonTop(props) {
-    const {name} = props;
-
+function ButtonTop({name}) {
     return (
         <div className="button__top">
             <div className="button__top__main">
